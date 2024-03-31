305f63f9bde91ac2658c83c12ffbbb674349a251
hammad910 <hammadfaisal0000yahoo@gmail.com>
2024-04-01 03:29:20 +0500

    .env created

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect(`${process.env.DATABASE_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`${process.env.DATABASE_URL}`);
